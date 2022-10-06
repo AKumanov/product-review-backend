f86f2b109e7c3210f6acd5ac50b5bd7b27c781f6
Alexander Kumanov <alexander.kumanov@dentap.local>
2022-10-06 16:18:05 +0300

    added jwt authentication - login and reg endpoints

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -78,14 +78,3 @@ class ImageSerializer(FlexFieldsModelSerializer):
     class Meta:
         model = Image
         fields = ('pk', 'name', 'image')
-
-
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-
-    @classmethod
-    def get_token(cls, user):
-        token = super(MyTokenObtainPairSerializer, cls).get_token(user)
-
-        # Add custom claims
-        token['username'] = user.username
-        return token
